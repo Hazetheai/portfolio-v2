fc4528e56332136a4f1ee950c4cbb54fe772ee64
Jake Riordan <contact@jakeriordan.dev>
2022-06-20 22:53:05 +0200

    feat(config): Set up base details

--- site.config.ts
+++ site.config.ts
@@ -2,24 +2,27 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'bc690c0b2bce4c599a7f00860e41ccf9',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Jake Riordan - Portfolio',
+  domain: 'jakeriordan.dev',
+  author: 'Jake Riordan',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Jake Riordan - Portfolio',
+  // socialImageTitle: 'Jake Riordan',
+  // socialImageSubtitle: 'Hello World! 👋',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'j_riordan',
+  github: 'hazetheai',
+  linkedin: 'jake-riordan-610ab1172',
+
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
